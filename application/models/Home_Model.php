6c10f5f117568db847ee14c918515cf7d3230a25
onomtam <matt.tam25@gmail.com>
2016-04-03 15:03:47 -0400

    Landing Page: Random Function [Incomplete]
    Loading random movies to the front page's jumbotron.

---
+++ application/models/Home_Model.php
@@ -0,0 +1,13 @@
+<?php
+class Home_model extends CI_Model {
+
+        public function __construct()
+        {
+                $this->load->database();
+        }
+
+        public function rand_movie(){
+                $query = $this->db->query('SELECT * FROM movie ORDER BY RANDOM() LIMIT 3');
+                return $query->result_array();
+        }
+
