5d8c0f03bcadab8db28bd679d1cb41d46591a2cb
onomtam <matt.tam25@gmail.com>
2016-04-06 18:42:20 -0400

    Movie Details 2

--- application/views/pages/movie.php
+++ application/views/pages/movie.php
@@ -1,11 +1,21 @@
+	<body background=<?php echo base_url('dep/img/Movie/Batman_vs_Superman/Batman_vs_Superman.jpg') ?>>
+	<style>
+	div.searchbox {
+	    background-color:#e7e7e7;
+	    color:black;
+	    margin:20px;
+	    padding:20px;
+	} 
+	</style>
 	<br>
 	<br>
 	<br>
 	<br>
 	<br>
 <div>
-	<body background=<?php echo base_url('dep/img/Movie/Batman_vs_Superman/Batman_vs_Superman.jpg') ?>>
+
 	<?php foreach ($results as $val): ?>
+	<div class="searchbox">
 		<h1><?php echo $val['name']?></h1>
 		<!-- <img class="img-responsive" src= <?php// echo base_url('dep/img/Movie/Batman_vs_Superman/Batman_vs_Superman.jpg') ?> alt=""> -->
 	<?php endforeach; ?>
