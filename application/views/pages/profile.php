b6310137c660e7fe22fd1e9c6b7c256966b7bdc6
onomtam <matt.tam25@gmail.com>
2016-04-10 21:35:21 -0400

    Dank Profiles

--- application/views/pages/profile.php
+++ application/views/pages/profile.php
@@ -13,7 +13,15 @@
 	<br>
 	<br>
 <?php foreach ($userdata as $userdata_val): ?>
-	<?php $username = $userdata_val['username'] ?>
+	<?php $username = $userdata_val['username'];
+		  $fname    = $userdata_val['fname'];
+		  $lname    = $userdata_val['lname'];
+		  $email    = $userdata_val['email'];
+		  $country    = $userdata_val['country'];		  
+
+
+	?>
+
 <?php endforeach; ?>
 <div class="container">
 	<div class="row">
@@ -23,6 +31,10 @@
 			</div>
 			<div class="col-md-9">
 				<h1> <?php echo $username ?> </h1>
+				<h3>Name: <?php echo $fname ?> <?php echo $lname ?> </h3>
+				<h3>Email:  <?php echo $email ?> </h3>
+				<h3>Country:  <?php echo $country ?> </h3>
+
 			</div>
 			
 		</div>
